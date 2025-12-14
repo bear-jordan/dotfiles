78115029f57716c3dc421b1196eb53759c9ef9cd
bear-jordan <bearjordan@gmail.com>
2025-12-13 21:29:15 -0500

    maint; update aliases

--- private_dot_config/bash/alias.sh
+++ private_dot_config/bash/alias.sh
@@ -2,11 +2,8 @@ alias ls="ls --color"
 alias c="clear"
 alias ll="ls -al"
 alias pytree="tree -I venv -I __pycache__"
-alias ff="fastfetch"
 alias tms="tmux-sessionizer"
 alias sb="source ~/.bashrc"
-alias k="kubectl"
-alias a="arduino-cli"
 
 function cd_up() {
     cd $(printf "%0.s../" $(seq 1 $1))
